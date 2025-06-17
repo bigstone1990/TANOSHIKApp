3b75abd1da113e382551ec21dcdbdad40e38739d
bigstone1990 <masatakalman@gmail.com>
2025-06-17 11:21:32 +0900

    GuestLayout アプリケーションロゴのクラス名一部変更

--- resources/js/Layouts/GuestLayout.tsx
+++ resources/js/Layouts/GuestLayout.tsx
@@ -13,7 +13,7 @@ export default function Guest({ children }: PropsWithChildren) {
                 <div className="flex w-full max-w-sm flex-col gap-6">
                     <div className="flex items-center gap-2 self-center font-medium">
                         <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
-                            <ApplicationLogo className="h-20 w-20 fill-current text-gray-500" />
+                            <ApplicationLogo className="fill-current text-gray-500" />
                         </div>
                         {appName}
                     </div>
