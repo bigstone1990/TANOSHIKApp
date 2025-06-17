0e19a0c8e0239e5fa7ed6992c85955aa40d14e80
bigstone1990 <masatakalman@gmail.com>
2025-06-18 07:06:24 +0900

    ダッシュボードとユーザー設定のページ内の文言変更

--- resources/js/Pages/Admin/Profile/Edit.tsx
+++ resources/js/Pages/Admin/Profile/Edit.tsx
@@ -29,14 +29,8 @@ export default function Edit() {
                         <Separator orientation="vertical" className="mr-2 h-4" />
                         <Breadcrumb>
                             <BreadcrumbList>
-                                <BreadcrumbItem className="hidden md:block">
-                                    <BreadcrumbLink href="#">
-                                        Building Your Application
-                                    </BreadcrumbLink>
-                                </BreadcrumbItem>
-                                <BreadcrumbSeparator className="hidden md:block" />
                                 <BreadcrumbItem>
-                                    <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+                                    <BreadcrumbPage>ユーザー設定</BreadcrumbPage>
                                 </BreadcrumbItem>
                             </BreadcrumbList>
                         </Breadcrumb>
