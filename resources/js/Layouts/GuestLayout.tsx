844d4d59257c8b23e5c04f544cecdae784e1d047
bigstone1990 <masatakalman@gmail.com>
2025-06-19 18:16:49 +0900

    アプリケーションロゴのサイズ変更

--- resources/js/Layouts/GuestLayout.tsx
+++ resources/js/Layouts/GuestLayout.tsx
@@ -9,8 +9,8 @@ export default function Guest({ children }: PropsWithChildren) {
             <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
                 <div className="flex w-full max-w-sm flex-col gap-6">
                     <div className="flex items-center gap-2 self-center font-medium">
-                        <div className="flex w-80 items-center justify-center rounded-md">
-                            <ApplicationLogo />
+                        <div className="flex justify-center">
+                            <ApplicationLogo className="w-80 h-auto"/>
                         </div>
                     </div>
                     {children}
--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -302,7 +302,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
           <SidebarMenuItem>
             <SidebarMenuButton size="lg" asChild>
               <Link href={isAdmin ? route('admin.top') : route('user.top')}>
-                <ApplicationLogo className={isMobile ? "!w-64 !h-12" : "!w-52 !h-12"} />
+                <ApplicationLogo className={isMobile ? "!w-64 !h-auto" : "!w-52 !h-auto"} />
               </Link>
             </SidebarMenuButton>
           </SidebarMenuItem>
